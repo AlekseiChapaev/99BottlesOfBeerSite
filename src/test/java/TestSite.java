1f05081c618b8fbce1636e4c8cec3f0436cf8a52
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-29 18:59:45 +0500

    add testCheckLanguage_TC_12_02, add getLinkName()

--- src/test/java/TestSite.java
+++ src/test/java/TestSite.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class TestSite{
 
@@ -21,16 +24,31 @@ public class TestSite{
         driver.quit();
     }
 
+    public String getLinkName(String letter){
+        return "//a[@href = '" + letter.toLowerCase() + ".html']";
+    }
+
     private static final String URL = "http://www.99-bottles-of-beer.net/";
     private static final String URL_BROWSE_LANGUAGE_BUTTON = "//li/a[contains(text(), 'Browse Languages')]";
 
     @Test
-    public void testCheckLanguage(){
+    public void testCheckLanguage_TC_12_01(){
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
-        driver.findElement(By.xpath("//a[@href = 'j.html']")).click();
+        driver.findElement(By.xpath(getLinkName("J"))).click();
 
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id = 'main']/p[1]")).getText(),
                 "All languages starting with the letter J are shown, sorted by Language.");
+    }
+
+    @Test
+    public void testCheckLanguage_TC_12_02(){
+        driver.get(URL);
+        driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
+        driver.findElement(By.xpath(getLinkName("M"))).click();
+        List<WebElement> languages = driver.findElements(By.xpath("//table[@id = 'category']/tbody/tr"));
+        String[] result = languages.get(languages.size() - 1).getText().split(" ");
+
+        Assert.assertEquals(result[0], "MySQL");
     }
 }
