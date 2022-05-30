75ee53afcf5661480757f8d1eb9cf90976160938
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-30 19:27:45 +0500

    add testCheckGuestBook_TC_12_06

--- src/test/java/TestSite.java
+++ src/test/java/TestSite.java
@@ -24,8 +24,12 @@ public class TestSite {
         driver.quit();
     }
 
-    public String getLinkName(String letter) {
+    public String setLinkName(String letter) {
         return "//a[@href = '" + letter.toLowerCase() + ".html']";
+    }
+
+    public void inputValueGuestBookPage(String field, String value){
+        driver.findElement(By.xpath("//input[@name = '" + field.toLowerCase() + "']")).sendKeys(value);
     }
 
     private static final String URL = "http://www.99-bottles-of-beer.net/";
@@ -35,7 +39,7 @@ public class TestSite {
     public void testCheckLanguage_TC_12_01() {
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
-        driver.findElement(By.xpath(getLinkName("J"))).click();
+        driver.findElement(By.xpath(setLinkName("J"))).click();
 
         Assert.assertEquals(driver.findElement(By.xpath("//div[@id = 'main']/p[1]")).getText(),
                 "All languages starting with the letter J are shown, sorted by Language.");
@@ -45,7 +49,7 @@ public class TestSite {
     public void testCheckLanguage_TC_12_02() {
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
-        driver.findElement(By.xpath(getLinkName("M"))).click();
+        driver.findElement(By.xpath(setLinkName("M"))).click();
         List<WebElement> languages = driver.findElements(By.xpath("//table[@id = 'category']/tbody/tr"));
         String[] result = languages.get(languages.size() - 1).getText().split(" ");
 
@@ -68,7 +72,7 @@ public class TestSite {
     public void testSolutionMathematica_TC_12_04() {
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
-        driver.findElement(By.xpath(getLinkName("M"))).click();
+        driver.findElement(By.xpath(setLinkName("M"))).click();
         driver.findElement(By.xpath("//a[. = 'Mathematica']")).click();
 
         Map<String, String> expectedResult = new HashMap<>();
@@ -91,7 +95,7 @@ public class TestSite {
     public void testCheckNumbersLanguages_TC_12_05() {
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
-        driver.findElement(By.xpath(getLinkName("0"))).click();
+        driver.findElement(By.xpath(setLinkName("0"))).click();
         List<WebElement> tDs = driver.findElements(By.cssSelector("#category tbody tr"));
 
         int count = 0;
@@ -102,5 +106,20 @@ public class TestSite {
         }
 
         Assert.assertEquals(count, 10);
+    }
+
+    @Test
+    public void testCheckGuestBook_TC_12_06() throws InterruptedException {
+        driver.get("http://www.99-bottles-of-beer.net/signv2.html");
+        inputValueGuestBookPage("Name", "Aleksei");
+        inputValueGuestBookPage("Location", "Perm");
+        inputValueGuestBookPage("email", "test@gmail.com");
+        inputValueGuestBookPage("homepage", "test.com");
+        inputValueGuestBookPage("captcha", String.valueOf((int)(Math.random() * 1000)));
+        driver.findElement(By.xpath("//textarea[@name = 'comment']")).sendKeys("Hi all!");
+        driver.findElement(By.xpath("//input[@type = 'submit']")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("#main p")).getText(),
+                "Error: Error: Invalid security code.");
     }
 }
