2bc3df7d7495af5a4b90f7ca38caa21fcabdfb24
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-30 18:03:43 +0500

    add testSolutionMathematica_TC_12_04

--- src/test/java/TestSite.java
+++ src/test/java/TestSite.java
@@ -8,11 +8,13 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.sql.DriverManager.getDriver;
 
-public class TestSite{
+public class TestSite {
 
     private WebDriver driver;
 
@@ -27,7 +29,7 @@ public class TestSite{
         driver.quit();
     }
 
-    public String getLinkName(String letter){
+    public String getLinkName(String letter) {
         return "//a[@href = '" + letter.toLowerCase() + ".html']";
     }
 
@@ -35,7 +37,7 @@ public class TestSite{
     private static final String URL_BROWSE_LANGUAGE_BUTTON = "//li/a[contains(text(), 'Browse Languages')]";
 
     @Test
-    public void testCheckLanguage_TC_12_01(){
+    public void testCheckLanguage_TC_12_01() {
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
         driver.findElement(By.xpath(getLinkName("J"))).click();
@@ -45,7 +47,7 @@ public class TestSite{
     }
 
     @Test
-    public void testCheckLanguage_TC_12_02(){
+    public void testCheckLanguage_TC_12_02() {
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
         driver.findElement(By.xpath(getLinkName("M"))).click();
@@ -56,14 +58,37 @@ public class TestSite{
     }
 
     @Test
-    public void testCheckHeaders_TC_12_03(){
+    public void testCheckHeaders_TC_12_03() {
         driver.get(URL);
         driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
         List<WebElement> headers = driver.findElements(By.cssSelector("#category tbody tr th"));
         String[] expectedResult = new String[]{"Language", "Author", "Date", "Comments", "Rate"};
 
-        for(int i = 0; i < headers.size(); i++){
+        for (int i = 0; i < headers.size(); i++) {
             Assert.assertEquals(headers.get(i).getText(), expectedResult[i]);
         }
+    }
+
+    @Test
+    public void testSolutionMathematica_TC_12_04() {
+        driver.get(URL);
+        driver.findElement(By.xpath(URL_BROWSE_LANGUAGE_BUTTON)).click();
+        driver.findElement(By.xpath(getLinkName("M"))).click();
+        driver.findElement(By.xpath("//a[. = 'Mathematica']")).click();
+
+        Map<String, String> expectedResult = new HashMap();
+        expectedResult.put("Comments", "1");
+        expectedResult.put("Date", "03/16/06");
+        expectedResult.put("Author", "Brenton Bostick");
+
+        List<WebElement> tDs = driver.findElements(By.xpath("//div[@id = 'main']/table/tbody/tr/td"));
+        Map<String, String> actualResult = new HashMap();
+        for (int i = 0; i < tDs.size() - 1; i += 2) {
+            actualResult.put(tDs.get(i).getText(), tDs.get(i + 1).getText());
+        }
+
+        Assert.assertEquals(actualResult.get("Author:"), expectedResult.get("Author"));
+        Assert.assertEquals(actualResult.get("Date:"), expectedResult.get("Date"));
+        Assert.assertEquals(actualResult.get("Comments:"), expectedResult.get("Comments"));
     }
 }
